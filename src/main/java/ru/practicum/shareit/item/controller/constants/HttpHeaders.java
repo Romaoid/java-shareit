1365b85fa76a050d3f4aa683177dc9ed427357b5
Romaoid <Romaoid@yandex.ru>
2026-04-08 12:08:57 +0300

    #4 add HttpHeaders - constant class. -1. Create constant class; -2. ItemController - fix request headers; -3. codestyle.

--- src/main/java/ru/practicum/shareit/item/controller/constants/HttpHeaders.java
+++ src/main/java/ru/practicum/shareit/item/controller/constants/HttpHeaders.java
@@ -1,6 +1,8 @@
 package ru.practicum.shareit.item.controller.constants;
 
 public final class HttpHeaders {
-    private HttpHeaders() {}
+    private HttpHeaders() {
+    }
+
     public static final String X_SHARER_USER_ID = "X-Sharer-User-Id";
 }
